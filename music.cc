57177ea7e1a568e90f27752a693eb01306ccf4fe
YafetDariel <71723825+YafetDariel@users.noreply.github.com>
2020-09-22 14:19:08 -0400

    Add files via upload

---
+++ music.cc
@@ -0,0 +1,48 @@
+<html>
+	<head>
+	<html>
+	<head>
+		<title>This is Guatemala</title>
+		<link type="text/css" rel="stylesheet" href="styles.css"/>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+	  <header>
+    <nav>
+        <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="attractions.html">Places to visit</a></li>
+        <li>Music</li>
+        <li><a href="food.html">Things to eat</a></li>
+        </ul>
+    </nav>
+	  </header>
+	  Music of Guatemala:
+	  <main>
+	  <li>Merengue</li>
+<li>Marimba music</li>
+<li>Garifuna music</li>
+<li>Punta</li>
+<li>Folk music</li>
+<li>Hip Hop</li>
+<li>Reggaeton</li>
+<li>Rock</li>
+<li>Classical music</li>
+<li>Amerindian music</li>
+	</head>
+	<body>
+	  <header>
+	    <a href="https://en.wikipedia.org/wiki/Music_of_Guatemala">Click here</a>
+	    Para mas informacion
+	  </header>
+	  <main>
+	    
+	    
+	    
+	  </main>
+	  <footer>
+	    
+	  </footer>
+	</body>
+</html>
+
